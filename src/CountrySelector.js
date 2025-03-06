4f6456fb0dc50f1c2cbfa703ed96ec7d9712b3dc
Nidhi <ndhpanchal1034@gmail.com>
2025-03-07 00:01:50 +0530

    update

--- src/CountrySelector.js
+++ src/CountrySelector.js
@@ -105,7 +105,7 @@ function CountrySelector (){
 
          {selectedCities && (<h2><span><strong>You selected </strong></span><span style={{fontSize:"40px"}}>{selectedCities}</span>,
          <span style={{
-             color: "#777",
+             color: "#777"
         
          }}>{" "}
             {selectedStates},{selectedCountries}</span> 
